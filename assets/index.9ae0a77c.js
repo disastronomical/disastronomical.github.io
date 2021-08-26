1960ec47ff591487fcf9d6d5035698a591f1ebaa
disastronomical <68868083+disastronomical@users.noreply.github.com>
2021-08-25 22:31:51 -0500

    Add files via upload

---
+++ assets/index.9ae0a77c.js
@@ -0,0 +1,1 @@
+const e=document.querySelector("#mobile-menu"),t=document.querySelector(".navbar__menu");e.addEventListener("click",(()=>{e.classList.toggle("is-active"),t.classList.toggle("active")}));
